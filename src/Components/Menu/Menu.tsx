0870d697a60a65dcbcf75f273071c69a05b148f5
thomas_girard45 <54374885+dalton-x@users.noreply.github.com>
2024-05-19 12:28:22 +0200

    Séparation en plusieurs components de la navbar pour evité des rendu inutiles au clique sur la navigation

---
+++ src/Components/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+function Menu( )
+{
+  return (
+    <div className="ks-logo">
+      <Link to="" >
+        <img src="src\assets\logo.png" alt="Logo" />
+      </Link>
+    </div>
+  )
+}
+
+export default Menu
